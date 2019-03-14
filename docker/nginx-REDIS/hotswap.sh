55721549b51f0c10e0912447d3e513f570744f85
Jonathan <joacheng@ucdavis.edu>
2019-03-14 04:56:54 +0000

    HW4

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 PARAM="$1"
 
-if [ $PARAM ==  "redis" ]; then
-    sed -i "11s/.*/    server %{REDIS_HOST}:%{REDIS_PORT};/" /etc/nginx/nginx.conf
-    sed -i "15s/.*/    listen %{PROXY_PORT} so_keepalive=on;/" /etc/nginx/nginx.conf
-    echo "original!"
+line=$(awk '/6379;/{print}' /etc/nginx/nginx.conf)
+
+server_name=$(echo $line | cut -d ':' -f 1)
+
+if [ "server $PARAM" == "$server_name" ]; then
+    echo "The file is already pointing to it."
 else
     sed -i "11s/.*/    server $PARAM:6379;/" /etc/nginx/nginx.conf
     sed -i "15s/.*/    listen 8888 so_keepalive=on;/" /etc/nginx/nginx.conf
-    echo $PARAM
 fi
 
 /usr/sbin/nginx -s reload
